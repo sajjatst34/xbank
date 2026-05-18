aa1f6e3650df2e48ef878b6bc13b71693a76637f
sajjatst34 <sajjatst@gmail.com>
2026-05-18 17:11:07 +0600

    Fix deployment

--- xbank/settings.py
+++ xbank/settings.py
@@ -16,10 +16,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-xbank-secret')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv(
-    'ALLOWED_HOSTS',
-    'localhost,127.0.0.1'
-).split(',')
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 INSTALLED_APPS = [
     'django.contrib.admin',
