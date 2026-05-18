f4130911268ec1b0f121db34ef80ff5459e2a3c4
sajjatst34 <sajjatst@gmail.com>
2026-05-18 19:47:25 +0600

    Send Mail

--- banking/utils.py
+++ banking/utils.py
@@ -88,7 +88,7 @@ X Bank Security Team
             message=message,
             from_email=settings.DEFAULT_FROM_EMAIL,
             recipient_list=[user.email],
-            fail_silently=False,
+            fail_silently=True,
         )
     except Exception as e:
         logger.error(f"Failed to send OTP email to {user.email}: {e}")
