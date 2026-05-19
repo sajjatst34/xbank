bbe11e908aa13720aa63a8323d4c184a1cdf0183
আপনার ইউজারনেম < >
2026-05-20 02:42:41 +0600

    Add Resend email service

--- xbank/settings.py
+++ xbank/settings.py
@@ -137,7 +137,7 @@ AXES_LOCKOUT_PARAMETERS = ['username', 'ip_address']
 
 EMAIL_BACKEND = "django.core.mail.backends.dummy.EmailBackend"
 
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'dummy.gmail.com'
 EMAIL_PORT = 587
 
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
